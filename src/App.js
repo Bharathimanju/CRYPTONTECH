3c0d71bad00b4aa387d7b685c213dd28cad6fc64
Manjubharathi <166109876+Bharathimanju@users.noreply.github.com>
2025-03-03 15:09:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,111 @@
+
+import React from "react";
+import "./App.css"
+// import iconr from "./images/iconr.png";
+
+
+
+// const CourseApp = () => {
+//     return (
+//         <div className="course-container">
+//             <h1 > My Courses</h1>
+//             <table className="course-table">
+//                 <thead>
+//                     <tr>
+//                         <th>Course Name</th>
+//                         <th>Start Date</th>
+//                         <th>Lessons Completed</th>
+//                         <th>Duration</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <td className="course-name">
+//                             <img src={iconr} alt="Basic Algorithm" className="course-image" />
+//                             <div className="course-info">
+//                                 <span>Basic Algorithm</span><br></br>
+//                                 <span className="course-lessons">25 Lessons</span>
+//                             </div>
+//                         </td>
+                        
+//                         <td>27 jan, 2023</td>
+//                         <td>10/25(48%)</td>
+//                         <td>14 38m 56s</td>
+//                     </tr>
+//                     <tr>
+//                         <td className="course-name">
+//                             <img src={iconr} alt="Basic Algorithm" className="course-image" />
+//                             <div className="course-info">
+//                                 <span>Web Development</span><br></br>
+//                                 <span className="course-lessons">45 lessons Lessons</span>
+//                             </div>
+//                         </td>
+//                         <td>23 feb, 2023</td>
+//                         <td>40/45 (97%)</td>
+//                         <td>14 38m 56s</td>
+//                     </tr>
+//                     <tr>
+//                         <td className="course-name">
+//                             <img src={iconr} alt="Basic Algorithm" className="course-image" />
+//                             <div className="course-info">
+//                                 <span>Basic Dtat science</span><br></br>
+//                                 <span className="course-lessons">37 Lessons</span>
+//                             </div>
+//                         </td>
+//                         <td>14 jan, 2023</td>
+//                         <td>9/37(40%)</td>
+//                         <td>2 weeks</td>
+//                     </tr>
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+// };
+
+// export default CourseApp;
+
+
+const functionapp=()=>{
+    return(
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col col-sm-12 col-md-12">
+                <h1> latest transactions</h1>
+                </div>
+                
+            </div>
+        </div>
+
+        <div className="container-fluid">
+            <div className="row">
+                <div className=" col col-sm-12 col-md-6 ">
+                    <div className="col" style={{justifyContent:"between"}}>
+                        <img src={{}}></img>
+                        <h3>aokiji</h3>
+                        <p>28 feb 2023</p>
+
+                    </div>
+                    <div className="col col-sm-12 col-md-6">
+                    <h3>235</h3>
+                    <p>recived</p>
+
+                </div>
+
+                </div>
+               
+
+            </div>
+
+        </div>
+
+
+
+
+        </>
+    )
+
+}
+export default functionapp
+
+
